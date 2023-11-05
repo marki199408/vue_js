abab9d5a73769e0873bba4f71c4a49c7dfcdf917
marki199408 <56339055+marki199408@users.noreply.github.com>
2023-11-05 20:28:10 +0800

    first and completed

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "./components/Navbar.vue";
+import TaskPage from "./pages/TaskPage.vue";
+import Footer from "./components/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <TaskPage />
+  <Footer />
+</template>
+
+<style scoped>
+</style>
